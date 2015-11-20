e4dbb6aae2b5f4dd5e50518a5b20af2e919cd293
Alex Danvy <alex@danvy.tv>
2015-11-20 02:37:30 +0000

    2.2 merge fix

--- Ch9Downloader.ps1
+++ Ch9Downloader.ps1
@@ -122,11 +122,9 @@ function New-Ch9Events()
     $events += $event
 
     $event = New-Ch9Event "Visual Studio"
-<<<<<<< HEAD
     $edition = New-Ch9EventEdition "Connect(); // 2015" "Connect2015" "https://channel9.msdn.com/Events/Visual-Studio/Connect-event-2015/RSS/"
-=======
+    $event.Editions += $edition
     $edition = New-Ch9EventEdition "Visual Studio 2015 Final Release Event" "VS2015FinalRelease" "https://channel9.msdn.com/Events/Visual-Studio/Visual-Studio-2015-Final-Release-Event/RSS/"
->>>>>>> bf9332305b599120c6b610762d01a44ef91e59a5
     $event.Editions += $edition
     $edition = New-Ch9EventEdition "Connect(); Microsoft Visual Studio vNext & Azure" "Connect2014" "http://channel9.msdn.com/Events/Visual-Studio/Connect-event-2014/RSS/"
     $event.Editions += $edition
@@ -235,11 +233,7 @@ function Ch9Download([string]$RssLink, [string]$DestFolder, [string]$Extension)
         DownloadFile $item.enclosure.url $fullFile
     }
 }
-<<<<<<< HEAD
-"Channel9 Content Downloader 2.1 by Alex Danvy @danvy"
-=======
 "Channel9 Content Downloader 2.2 by Alex Danvy @danvy"
->>>>>>> bf9332305b599120c6b610762d01a44ef91e59a5
 "Source code available on http://github.com/danvy/channel9"
 #Event
 "Select the event on Channel9:"
